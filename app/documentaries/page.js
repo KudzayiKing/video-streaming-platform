9d0bc9cc60fdcb581888a9b523a4f40a01a253c8
Kudzayi King <itsswagg@gmail.com>
2023-02-07 22:49:15 +0300

    corrections

--- app/documentaries/page.js
+++
@@ -1,3 +0,0 @@
-export default function Page(){
-    return <p className='mt-12 text-white'>Documentaries Page</p>
-}
--- app/myList/page.js
+++
@@ -1,3 +0,0 @@
-export default function Page(){
-    return <p className='mt-12 text-white'>My List Page</p>
-}
